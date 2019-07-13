67bcd4749af17c0993f17f025919752b28e84f89
ThomazNexmo <thomazmanoel.aquino@gmail.com>
2019-07-14 00:12:53 +0200

    Angular material

---
+++ src/app/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatNativeDateModule
+  ],
+
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+})
+    
+export class MaterialModule { }
